6c2e509b064d73b9dde9a28b844e5a39f255f8ab
Matthias Gattermeier <matthias.gattermeier@gmail.com>
2015-08-28 10:23:39 -0700

    feat/middleware - adds bodyparser for urlencoding and json

--- server/routes/urlRouter.js
+++ server/routes/urlRouter.js
@@ -1,23 +1,26 @@
+var bodyParser = require('body-parser');
 var auth = require('../middleware').auth;
 
+var jsonParser = bodyParser.json();
+
 module.exports = function(app) {
-  app.get('/api/urls', auth.isAuthorized, function(req, res, next) {
+  app.get('/api/urls', jsonParser, auth.isAuthorized, function(req, res, next) {
     // Get list of urls
   });
 
-  app.get('/api/urls/:id', auth.isLoggedIn, function(req, res, next) {
+  app.get('/api/urls/:id', jsonParser, auth.isLoggedIn, function(req, res, next) {
     // Get individual url
   });
 
-  app.post('/api/urls', auth.isAdmin, function(req, res, next) {
+  app.post('/api/urls', jsonParser, auth.isAdmin, function(req, res, next) {
     // Add new url!
   });
 
-  app.put('/api/urls/:id', auth.isAdmin, function(req, res, next) {
+  app.put('/api/urls/:id', jsonParser, auth.isAdmin, function(req, res, next) {
     // Updates url!
   });
 
-  app.delete('/api/urls/:id', auth.isAdmin, function(req, res, next) {
+  app.delete('/api/urls/:id', jsonParser, auth.isAdmin, function(req, res, next) {
     // Delete a url!
   });
 }
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,23 +1,25 @@
+var bodyParser = require('body-parser');
 var auth = require('../middleware').auth;
 
+var jsonParser = bodyParser.json();
 module.exports = function(app) {
-  app.get('/api/users', auth.isAuthorized, function(req, res, next) {
+  app.get('/api/users', jsonParser, auth.isAuthorized, function(req, res, next) {
     // Get list of users
   });
 
-  app.get('/api/users/:id', auth.isAuthorized, function(req, res, next) {
+  app.get('/api/users/:id', jsonParser, auth.isAuthorized, function(req, res, next) {
     // Get individual user
   });
 
-  app.post('/api/users', auth.isAuthorized, function(req, res, next) {
+  app.post('/api/users', jsonParser, auth.isAuthorized, function(req, res, next) {
     // Add new user!
   });
 
-  app.put('/api/users/:id', auth.isAuthorized, function(req, res, next) {
+  app.put('/api/users/:id', jsonParser, auth.isAuthorized, function(req, res, next) {
     // Updates user!
   });
 
-  app.delete('/api/users/:id', auth.isAuthorized, function(req, res, next) {
+  app.delete('/api/users/:id', jsonParser, auth.isAuthorized, function(req, res, next) {
     // Delete a user!
   });
 }
