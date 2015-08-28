35c0688e7e40120507b7b7ef565e3b36c7e8c828
Dave Schumaker <daveschumaker@gmail.com>
2015-08-28 11:16:53 -0700

    Update watch task and add linter dependency.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,23 @@
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
+var tape = require('gulp-tape');
 
-gulp.task('default', function() {
-  // place code for your default task here
-  console.log("Hello!!");
+// TEST TASKS
+gulp.task('test', function() {
+  return gulp.src('test/*.js')
+    .pipe(tape());
 });
 
+// DEFAULT GULP TASKS
+gulp.task('default', function() {
+  // place code for your default task here
+  return gulp.src(['server/**/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish))
+});
+
+// WATCH TASK
 gulp.task('watch', function() {
-  gulp.watch('*', ['default']);
+  gulp.watch(['server/**/*.js', 'test/*.js'], ['test', 'default']);
 })
