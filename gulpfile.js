5bc4c9f6029e1eb78c65307213ad0017f055c836
Dave Schumaker <daveschumaker@gmail.com>
2015-08-28 12:16:34 -0700

    Break out some tasks in gulp file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var concat = require('gulp-concat');
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var notify = require("gulp-notify");
+var plumber = require('gulp-plumber');
 var stylish = require('jshint-stylish');
 var tape = require('gulp-tape');
 var uglify = require('gulp-uglify');
@@ -8,18 +10,27 @@ var uglify = require('gulp-uglify');
 // TEST TASKS
 gulp.task('test', function() {
   return gulp.src('test/*.js')
-    .pipe(tape());
+    .pipe(plumber({
+      errorHandler: notify.onError("Error: <%= error.message %>")
+    }))
+    .pipe(tape())
 });
 
-// DEFAULT GULP TASKS
-gulp.task('default', function() {
+// CHECK CODE
+gulp.task('linter', function() {
   // place code for your default task here
   return gulp.src(['server/**/*.js'])
+    .pipe(plumber({
+      errorHandler: notify.onError("Error: <%= error.message %>")
+    }))
     .pipe(jshint())
     .pipe(jshint.reporter(stylish))
 });
 
+// DEFAULT
+gulp.task('default', ['test', 'linter']);
+
 // WATCH TASK
 gulp.task('watch', function() {
   gulp.watch(['server/**/*.js', 'test/*.js'], ['test', 'default']);
-})
+});
