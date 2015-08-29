f0b721dbb5d3c2a8dd52a8e2f2a104caa0d83c83
Emily Eng <emily.w.eng@gmail.com>
2015-08-29 15:51:19 -0700

    Update auth routes + config example for OAuth.

--- server/config.example.js
+++ server/config.example.js
@@ -7,6 +7,16 @@ var config = {
       username: 'username',
       password: ''
     },
+    googleAuth: {
+      clientId: 'our-client-id-here',
+      clientSecret: 'our-client-secret-here',
+      callbackUrl: 'http://localhost:3000/api/auth/google/callback'
+    },
+    facebookAuth: {
+      clientId: 'our-client-id-here',
+      clientSecret: 'our-client-secret-here',
+      callbackUrl: 'http://localhost:3000/api/auth/facebook/callback'
+    },
     secret: 'development'
   }
 };
