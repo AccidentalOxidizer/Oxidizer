2e85efcbd295c2103a53970143cf4219e7ad5a43
Dave Schumaker <daveschumaker@gmail.com>
2015-08-29 12:02:37 -0700

    Build out URL controller.

---
+++ server/components/url/urlController.js
@@ -0,0 +1,56 @@
+var Url = require('../').Url;
+
+/** 
+ *  This controller is responsible for getting and 
+ *  adding URLs from specific webpages to our database.
+ */
+
+// GET a URL from the database
+var get = function(searchObject) {
+  return Url.findOne({
+    where: searchObject
+  });
+};
+
+// Write a new URL to the database
+var post = function(urlObject) {
+  return User.findOrCreate({
+      where: urlObject
+    })
+    .spread(function(url, created) {
+      if (!created) {
+        throw new Error('Error posting to db');
+      } else {
+        return url;
+      }
+    });
+};
+
+// DELETE a URL from the database.
+// TODO: We should also probably delete any 
+// associated comments from the database as well.
+var remove = function(urlId) {
+  return Url.destroy({
+      where: {
+        id: urlId
+      }
+    })
+    .then(function(affectedRows) {
+      console.log(affectedRows);
+      if (affectedRows === 0) {
+        throw new Error('Url not found - delete failed');
+      } else if (affectedRows > 1) {
+        throw new Error('deleted multiple url');
+      } else {
+        return true;
+      }
+    })
+    .catch(function(error) {
+      throw new Error(error);
+    });
+};
+
+
+exports.get = get;
+exports.post = post;
+exports.remove = remove;
