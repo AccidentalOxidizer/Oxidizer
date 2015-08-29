e02937ddc730d651b4ee9277ae927b452599bde7
eliotwinder <musicismymistress@gmail.com>
2015-08-29 15:10:39 -0700

    (feat)move db relationships

--- server/components/index.js
+++ server/components/index.js
@@ -16,4 +16,63 @@ for (var k in models) {
   module.exports[k] = sequelize.import(__dirname + '/' + models[k]);
 }
 
-module.exports.sequelize = sequelize
+(function(m) {
+  m.Comment.belongsTo(m.User, {
+    foreignKey: {
+      allowNull: false
+    },
+    onDelete: 'cascade'
+  });
+  m.User.hasMany(m.Comment);
+
+  m.Group.belongsToMany(m.User, {through: 'UserGroup'});
+  m.User.belongsToMany(m.Group, {through: 'UserGroup'});
+
+  m.Heart.belongsTo(m.User, {
+    foreignKey: {
+      allowNull: false
+    },
+    onDelete: 'cascade'
+  });
+  m.User.hasMany(m.Heart);
+
+  m.Flag.belongsTo(m.User, {
+    foreignKey: {
+      allowNull: false
+    },
+    onDelete: 'cascade'
+  });
+  m.User.hasMany(m.Flag);
+
+  m.Flag.belongsTo(m.Comment, {
+    foreignKey: {
+      allowNull: false
+    },
+    onDelete: 'cascade'
+  });
+  m.Comment.hasMany(m.Flag);
+
+  m.Heart.belongsTo(m.Comment, {
+    foreignKey: {
+      allowNull: false
+    },
+    onDelete: 'cascade'
+  });  
+  m.Comment.hasMany(m.Heart);
+
+
+  m.Comment.belongsTo(m.Group);
+  m.Group.hasMany(m.Comment);  
+
+  m.Comment.belongsTo(m.Url, {
+    foreignKey: {
+      allowNull: false
+    },
+    onDelete: 'cascade'
+  });
+  m.Url.hasMany(m.Comment);
+
+})(module.exports);
+
+module.exports.Sequelize = Sequelize;
+module.exports.sequelize = sequelize;
