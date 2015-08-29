26d9ee423385962b3283ec6430118de0727ed9d6
eliotwinder <musicismymistress@gmail.com>
2015-08-29 14:57:19 -0700

    commentController

---
+++ server/components/comment/commentController.js
@@ -0,0 +1,55 @@
+var Comment = require('../').Comment;
+
+var get = function(searchObject){
+  return Comment.findAll({where: searchObject});
+};
+
+// takes an object with the following format
+var post = function(commentObject){
+  console.log('//////////////', commentObject);
+  var newComment = Comment.build(commentObject);
+  console.log('$$$$$$$$$$$$$$$$$$$$$$$$$$',Comment);
+  return newComment.save() 
+    .then(function(comment){
+      return comment;
+    })
+    .catch(function(err){
+      throw err;
+    });
+};
+
+// will throw error if no id provided
+var put = function(commentid, updatesObject){
+  return Comment.update(updatesObject, {where: {id: commentid}})
+    .then(function(){
+      return true;
+    })
+    .catch(function(error){
+      throw new Error(error);
+    });  
+};
+
+var remove = function(commentId){
+  return Comment.destroy({where: {id: commentId}})
+    .then(function(affectedRows){
+      console.log(affectedRows);
+      if (affectedRows === 0){
+        throw new Error('User not found - delete failed');
+      } else if (affectedRows > 1) {
+        throw new Error('deleted multiple users');
+      } else {
+        return true;
+      }
+    })
+    .catch(function(error) {
+      throw new Error(error);
+    });
+};
+
+
+exports.get = get;
+exports.post = post;
+exports.put = put;
+exports.remove = remove;
+
+
--- test/commentServerSpec.js
+++ test/commentServerSpec.js
@@ -1,5 +1,11 @@
 // IMPORT REQUIRED MODULES FROM SERVER
 var utils = require('../server/utils');
+var Comment = require('../server/components/comment');
+var commentModel = require('../server/components').Comment;
+var userModel = require('../server/components').User;
+var urlModel = require('../server/components').Url;
+var Promise = require('bluebird');
+var testHelpers = require('./testHelpers');
 
 // TEST MODULES
 var test = require('tape');
@@ -24,27 +30,93 @@ var test = require('tape');
  * node test/serverSpec.js
  */
 
-test('----- Comment Tests -----\n\n', function(t) {
+test('----- Comment Controller Tests -----\n\n', function(t) {
   t.plan(6); // Number of tests that we plan to run
 
-  // Create a new comment with a user account?
-  t.equal(200, 200, 'New comment added!');
+  var dummyComment = {
+    text: 'funny comment about the web',
+    isPrivate: false
+  };
 
-  // Try to post a comment containing a word from a potential spam blacklist
-  // We haven't actually created this yet. 
-  t.equal(200, 200, 'Comment with word from blacklist ignored!');
 
-  // Favorite a comment and make sure fav count increases?
-  t.equal(200, 200, 'Comment successfully favorited');
+  // save a user and url concurrently so that we can create a comment
+  var testCommentPost = function(dummyComment, t){
+    var testUserId;
+    var testUrlId;
 
-  // Flag a comment and make sure flag count increases?
-  t.equal(200, 200, 'Comment successfully flagged');
+    var dummyUser = {
+      name: 'eliotdummy',
+      email: 'eliot@eliot.com',
+      status: 1
+    };
+    
+    var dummyUrl = {
+      path: 'www.rustedserverwfunnycomments.io'
+    };
+    
+    var testUser = userModel.build(dummyUser);
+    var testUrl = urlModel.build(dummyUrl);
 
-  // Delete a comment (and associated favs / flags)
-  t.equal(410, 410, 'Comment successfully deleted');
+    return Promise.all([testUser.save(), testUrl.save()])
+      .then(function(data){
+        var testUser = data[0];
+        var testUrl = data[1];
+        
+        var dummyCommentCopy = {
+          text: dummyComment.text,
+          isPrivate: dummyComment.isPrivate,
+          UserId: {
+            id: testUser.get('id')
+          },
+          UrlId: {id: testUrl.get('id')}
+        };
 
-  // Try to post a comment as a banned / blocked user 
-  // (maybe comment is auto hidden)?
-  t.equal(500, 500, 'Banned account cannot comment');
-
+        return testHelpers.testPost(Comment.post, dummyCommentCopy, commentModel, t);
+      })
+      // cleanup everything
+      .then(function(){
+        return Promise.all([
+          userModel.destroy({where: {id: testUserId}}),
+          urlModel.destroy({where: {id: testUrlId}})
+        ])
+        .catch(function(err){
+          throw err;
+        });
+      })
+      .catch(function(err){        
+        userModel.destroy({where: {id: testUserId}});
+        urlModel.destroy({where: {id: testUrlId}});
+        throw err;
+      });
+  };
+  
+  testCommentPost(dummyComment, t)  
+    .catch(function(err){
+      throw err;
+    });
 });
+
+// test('----- Comment Router Tests -----\n\n', function(t) {
+//   t.plan(6); // Number of tests that we plan to run
+
+//   // Create a new comment with a user account?
+//   t.equal(200, 200, 'New comment added!');
+
+//   // Try to post a comment containing a word from a potential spam blacklist
+//   // We haven't actually created this yet. 
+//   t.equal(200, 200, 'Comment with word from blacklist ignored!');
+
+//   // Favorite a comment and make sure fav count increases?
+//   t.equal(200, 200, 'Comment successfully favorited');
+
+//   // Flag a comment and make sure flag count increases?
+//   t.equal(200, 200, 'Comment successfully flagged');
+
+//   // Delete a comment (and associated favs / flags)
+//   t.equal(410, 410, 'Comment successfully deleted');
+
+//   // Try to post a comment as a banned / blocked user 
+//   // (maybe comment is auto hidden)?
+//   t.equal(500, 500, 'Banned account cannot comment');
+
+// });
