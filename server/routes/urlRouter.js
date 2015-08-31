8be2c5017946121cc8262048faf2132db177ad45
Dave Schumaker <daveschumaker@gmail.com>
2015-08-31 16:25:01 -0700

    Update URL router

--- server/routes/urlRouter.js
+++ server/routes/urlRouter.js
@@ -1,5 +1,6 @@
 var bodyParser = require('body-parser');
 var auth = require('../middleware').auth;
+var url = require('url');
 var Promise = require('bluebird');
 var urlHelper = Promise.promisifyAll(require('../components/url'));
 
@@ -7,36 +8,53 @@ var jsonParser = bodyParser.json();
 
 module.exports = function(app) {
   //app.get('/api/urls', jsonParser, auth.isAuthorized, function(req, res, next) {
-  app.get('/api/urls', jsonParser, function(req, res, next) {
-    // Get list of urls
-    res.send(200, "Should return list of all urls.");
-  });
+  app.get('/api/urls*', jsonParser, function(req, res, next) {
+    // Get url info
+    var url_parts = url.parse(req.url, true);
+    var fullPath = url_parts.path;
 
-  //  app.get('/api/urls/:id', jsonParser, auth.isLoggedIn, function(req, res, next) {
-  app.get('/api/urls/:url', jsonParser, function(req, res, next) {
-    /**
-     * Get individual url as part of the query string.
-     * NOTE: When using this method, we must first
-     * ENCODE urls that get passed in as URLs using the
-     * encodeURIComponent function that's built into JS.
-     *
-     * This escapes any URL characters that are liable to break
-     * stuff. The drawback is that we need to take whatever parameter
-     * is passed into the query string and decode it back to a valid URL
-     * before we pass it into the database. That happens below.
-     *
-     * This is setup as an object since we specifically ask for a
-     * search object in our URL get controller.
-     */
+    //Remove our API path from the actual path so we can get our real URL.
+    var fullPath = fullPath.replace('/api/urls/', '');
+    console.log("FULL PATH: ", fullPath);
+
     var urlToGet = {
-      path: decodeURIComponent(req.params.url)
+      path: fullPath
     };
 
     urlHelper.get(urlToGet)
       .then(function(url) {
         res.send(200, url);
       })
+
+    //res.send(200, "Should return list of all urls.");
   });
+
+  //  app.get('/api/urls/:id', jsonParser, auth.isLoggedIn, function(req, res, next) {
+  //app.get('/api/urls/:url', jsonParser, function(req, res, next) {
+  /**
+   * Get individual url as part of the query string.
+   * NOTE: When using this method, we must first
+   * ENCODE urls that get passed in as URLs using the
+   * encodeURIComponent function that's built into JS.
+   *
+   * This escapes any URL characters that are liable to break
+   * stuff. The drawback is that we need to take whatever parameter
+   * is passed into the query string and decode it back to a valid URL
+   * before we pass it into the database. That happens below.
+   *
+   * This is setup as an object since we specifically ask for a
+   * search object in our URL get controller.
+   */
+  //   var urlToGet = {
+  //     path: decodeURIComponent(req.params.url)
+  //   };
+
+  //   urlHelper.get(urlToGet)
+  //     .then(function(url) {
+  //       res.send(200, url);
+  //     })
+  // });
+
 
   //app.post('/api/urls', jsonParser, auth.isAdmin, function(req, res, next) {
   app.post('/api/urls', jsonParser, function(req, res, next) {
