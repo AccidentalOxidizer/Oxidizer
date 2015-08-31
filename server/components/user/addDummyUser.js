f638ce81b3d35aaad4fe932103a93b2d9ac1ac23
eliotwinder <musicismymistress@gmail.com>
2015-08-31 14:42:10 -0700

    (feat) userRouter ready with testing closes #63

--- server/components/user/addDummyUser.js
+++ server/components/user/addDummyUser.js
@@ -1,7 +1,12 @@
 var User = require('./index');
 
-User.post({
-  name: 'e',
-  email: 'e@e.com',
-  password: 'e'
-});
+var alphabet = "abcdefeghjiklmnopqrstuvwxyz";
+
+for (var i = 0; i < alphabet.length; i++) {
+  var currentLetter = alphabet[i];
+  User.post({
+    name: currentLetter,
+    email: currentLetter + '@' + currentLetter,
+    password: currentLetter
+  });
+}
