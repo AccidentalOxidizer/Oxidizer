6198bff73b2aac3111c0c2babd5cef15f8eaeeed
Dave Schumaker <daveschumaker@gmail.com>
2015-08-31 12:15:52 -0700

    Add documentation explaining finer points of each of these methods.

--- server/routes/urlRouter.js
+++ server/routes/urlRouter.js
@@ -12,7 +12,20 @@ module.exports = function(app) {
 
   //  app.get('/api/urls/:id', jsonParser, auth.isLoggedIn, function(req, res, next) {
   app.get('/api/urls/:url', jsonParser, function(req, res, next) {
-    // Get individual url
+    /**
+     * Get individual url as part of the query string.
+     * NOTE: When using this method, we must first
+     * ENCODE urls that get passed in as URLs using the
+     * encodeURIComponent function that's built into JS.
+     *
+     * This escapes any URL characters that are liable to break
+     * stuff. The drawback is that we need to take whatever parameter
+     * is passed into the query string and decode it back to a valid URL
+     * before we pass it into the database. That happens below.
+     *
+     * This is setup as an object since we specifically ask for a
+     * search object in our URL get controller.
+     */
     var urlToGet = {
       path: decodeURIComponent(req.params.url)
     };
@@ -27,14 +40,17 @@ module.exports = function(app) {
 
   //app.post('/api/urls', jsonParser, auth.isAdmin, function(req, res, next) {
   app.post('/api/urls', jsonParser, function(req, res, next) {
-    // Add new url!
+    /**
+     * This is setup as an object since we specifically ask for a
+     * search object in our URL get controller.
+     */
     var urlToSave = {
       path: req.body.url
     }
 
-    //console.log(urlToSave);
-
-    // Encoded UR
+    // Encoded URL
+    // The ONLY reason this is currently here is for debugging purposes so I can
+    // get properly encoded URLs to search for later.
     var encodedURL = encodeURIComponent(urlToSave.path);
 
     // Save URL to database if it doesn't exist.
