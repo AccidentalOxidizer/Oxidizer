7251a626abd1ace5d059a838f8aceb3264dcb057
Matthias Gattermeier <matthias.gattermeier@gmail.com>
2015-09-02 12:09:39 -0700

    dy for production

--- server/components/index.js
+++ server/components/index.js
@@ -13,6 +13,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 
+
 var models = {
   'User': 'user/userModel',
   'Url': 'url/urlModel',
--- server/config.js
+++ server/config.js
@@ -1,11 +1,30 @@
 var config = {
-  production: {},
-  staging: {},
+  production: {
+    dbconfig: {
+      path: 'rust.cezxregsdvmw.us-west-2.rds.amazonaws.com',
+      name: 'rust',
+      username: 'root',
+      password: 'mypassword',
+      port: '3306'
+    },
+    googleAuth: {
+      clientId: '837204469183-nklfavnlll2v6jh0ku65tieuvc6qmhn1.apps.googleusercontent.com',
+      clientSecret: 'mXAPIt-gVJqr4NeXIvVFTi_M',
+      callbackUrl: 'http://localhost:3000/api/auth/google/callback'
+    },
+    facebookAuth: {
+      clientId: '996722297026435',
+      clientSecret: 'cf12933d9699db44e3577e54d2fda15e',
+      callbackUrl: 'http://localhost:3000/api/auth/facebook/callback'
+    },
+    secret: 'development'
+  },
   development: {
     dbconfig: {
       name: 'rust',
       username: 'root',
-      password: ''
+      password: '',
+      port: ''
     },
     googleAuth: {
       clientId: '837204469183-nklfavnlll2v6jh0ku65tieuvc6qmhn1.apps.googleusercontent.com',
@@ -22,7 +41,7 @@ var config = {
 }
 
 exports.get = function get(env) {
-  return config[env] || config.development;
+  return config[env] || config.production;
 }
 
 
