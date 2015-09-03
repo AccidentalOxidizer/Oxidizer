7c9ba17b5ff97267784a4796b5425ff7fba0914d
Dave Schumaker <daveschumaker@gmail.com>
2015-09-03 15:33:31 -0700

    Clean up JSON response from server.

--- server/components/comment/commentController.js
+++ server/components/comment/commentController.js
@@ -7,19 +7,33 @@ var get = function(searchObject) {
   var attributes = ['text', 'User.name'];
 
   return Comment.findAll({
-    where: searchObject,
-    // attributes: attributes,
-    include: [{
-      model: User,
-      attributes: ['name']
-    }, {
-      model: Heart,
-      attributes: ['id']
-    }, {
-      model: Flag,
-      attributes: ['id']
-    }]
-  });
+      where: searchObject,
+      // attributes: attributes,
+      include: [{
+        model: User,
+        attributes: ['name']
+      }, {
+        model: Heart,
+        attributes: ['id']
+      }, {
+        model: Flag,
+        attributes: ['id']
+      }]
+    })
+    .then(function(results) {
+      //console.log("GET COMMENT RESULTS: ", results);
+      // Iterate over our results array and update the number of hearts and favorites so
+      // we don't return the ENTIRE array.
+      results.forEach(function(element, index, array) {
+        results[index].dataValues.Hearts = results[index].dataValues.Hearts.length;
+        results[index].dataValues.Flags = results[index].dataValues.Flags.length;
+      });
+
+      return results;
+    })
+    .catch(function(err) {
+      console.log("Err getting comments: ", err);
+    });
 };
 
 // takes an object with the following format
