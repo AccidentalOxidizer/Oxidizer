c0a73cf2caf97d192b1b00c4f815d7008587650d
Dave Schumaker <daveschumaker@gmail.com>
2015-09-02 17:34:44 -0700

    Test index.html form can now get comments from any URL.

--- client/oxidizer.js
+++ client/oxidizer.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+  // Use this to build out HTML for individual comments.
   var buildComments = function(comment) {
     var commentHTML = '<p>' + comment.User.name + '</br>' +
       comment.createdAt + '</br>' + comment.text + '</p>';
@@ -19,33 +20,46 @@ $(document).ready(function() {
     $('#container').show();
   }
 
-  var data = JSON.stringify({
-    url: 'http://reddit.com/r/kerbalspaceprogram'
+  var getComments = function(url) {
+    url = url || 'http://reddit.com/r/kerbalspaceprogram'
+      // Default website to show comments from on page load.
+    var data = JSON.stringify({
+      url: url
+    });
+
+    // AJAX call to server to get comments from a particular URL.
+    $.ajax({
+      type: "POST",
+      url: 'http://localhost:3000/api/comments/get',
+      data: data,
+      contentType: 'application/json', // content type sent to server
+      dataType: 'json', //Expected data format from server
+      success: function(data) {
+        $('#comments').html('');
+
+        console.log('DATA: ', data);
+        console.log('DONE!');
+
+        var commentArrayHTML = '';
+        // Render comment HTML
+        data['comments'].forEach(function(element, index) {
+          commentArrayHTML = commentArrayHTML.concat(buildComments(data['comments'][index]));
+        });
+
+        $('#comments').html(commentArrayHTML);
+      },
+      error: function(err) {
+        console.log("Awww shit", err);
+      }
+    });
+  };
+
+  // On Page Load:
+  getComments();
+
+
+  $("#urlForm").submit(function() {
+    console.log('STATUS = BALLER');
+    getComments($('#getUrl').val());
   });
-
-  $.ajax({
-    type: "POST",
-    url: 'http://localhost:3000/api/comments/get',
-    data: data,
-    contentType: 'application/json', // content type sent to server
-    dataType: 'json', //Expected data format from server
-    success: function(data) {
-      console.log('DATA: ', data);
-      console.log('DONE!');
-
-      var commentArrayHTML = '';
-      // Render comment HTML
-      data['comments'].forEach(function(element, index) {
-        commentArrayHTML = commentArrayHTML.concat(buildComments(data['comments'][index]));
-      });
-
-      $('#comments').html(commentArrayHTML);
-    },
-    error: function(err) {
-      console.log("Awww shit", err);
-    }
-  });
-
-
-
 });
