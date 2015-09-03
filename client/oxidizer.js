dfff252d254311b225db477c701160cf7afed0de
Dave Schumaker <daveschumaker@gmail.com>
2015-09-02 19:12:20 -0700

    Add a bunch of comments to the code.

--- client/oxidizer.js
+++ client/oxidizer.js
@@ -1,15 +1,17 @@
 $(document).ready(function() {
 
   // Initial vars
+  // Setting this to emtpy so we can do some interesting stuff later.
   var url = '';
 
   // Use this to build out HTML for individual comments.
+  // Basically, our AJAX call will get data back, loop over the array of comments
+  // then send each individiaul comment here to build out the comment.
   var buildComments = function(comment) {
     var commentHTML = '<p>Comment ID:' + comment.id + '<br/>' + comment.User.name + '<br/>' +
       comment.createdAt + '<br/>' + comment.text + '<br/>' +
       '<a href="#">FAVE IT</a> || <a href="#">FLAG IT</a> <br/>' +
       'TOTAL FAVS: 0 || TOTAL FLAGS: 0</p>';
-
     return commentHTML;
   };
 
@@ -17,6 +19,8 @@ $(document).ready(function() {
   // NOTE: This isn't working at the moment.
   var isLoggedIn = true;
 
+  // This should show or hide the login / logout options depending on the logged in status
+  // of the user. This isn't working just yet though.
   if (isLoggedIn === false) {
     $('#login').show();
     $('#container').hide();
@@ -25,6 +29,9 @@ $(document).ready(function() {
     $('#container').show();
   }
 
+  // Wrapping our AJAX call to the server to get comments in a function. Why?
+  // That way we can update the server if I input a new URL to look at in the
+  // input box at the top of the screen.
   var getComments = function(url) {
     url = url || 'http://reddit.com/r/kerbalspaceprogram';
     // Default website to show comments from on page load.
@@ -60,10 +67,14 @@ $(document).ready(function() {
     });
   };
 
-  // On Initial Page Load:
+  // On Initial Page Load (Providing we're ultimately logged into Google):
+  // GET NEW COMMENTS!
+  // We don't need to pass in an initial URL since the getComments() function will
+  // check if anything exists, otherwise it's set to a default.
   getComments();
 
-
+  // This checks the URL FORM for any URLs and will call the getComments()
+  // function in order to get updated URLs.
   $("#urlForm").submit(function() {
     console.log('STATUS = BALLER');
     getComments($('#getUrl').val());
