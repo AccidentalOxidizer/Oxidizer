6bf10997d27084c7c105d43b34b6d0263c016fbe
Dave Schumaker <daveschumaker@gmail.com>
2015-09-03 19:44:32 -0700

    Working on Heart Controller
    In the midst of rewriting the heartController so that it first checks whether or not a user has already marked a particular comment as a favorite. If so, then we go ahead and DESTROY the favorite. If the user has not yet marked this particular comment as a favorite, then we go ahead and CREATE a new favorite. However, this is currently breaking our commentRouter and I'm not sure why.

--- server/components/heart/heartController.js
+++ server/components/heart/heartController.js
@@ -11,7 +11,7 @@ var get = function(searchObject) {
       where: searchObject
     })
     .then(function(result) {
-      //console.log(result);
+      //console.log('HEART GET line 14: ', result);
       return result;
     })
     .catch(function(err) {
@@ -21,12 +21,30 @@ var get = function(searchObject) {
 };
 
 var fave = function(searchObject) {
-  //console.log("FAVE OBJECT: ", searchObject);
-  //var newFave = Heart.build(searchObject);
-  return Heart.findOrCreate({
+  // Note: This needs to be findAll() instead of findOne(), because
+  // findAll() returns an array of things that we can count (whether 0, 1, or 1000);
+  Heart.findAll({
       where: searchObject
     })
-    .then(function(favorite) {
+    .then(function(result) {
+      if (result.length === 0) {
+        // No result found, let's go ahead and face it!
+        console.log("No faves found. Adding new favs!");
+        var newFave = Heart.build(searchObject);
+        return newFave.save();
+      } else {
+        // Hey, this punk already had a favorite for
+        // this item. Let's delete it!
+        console.log('Fave already found. Removing!');
+        return Heart.destroy({
+          where: searchObject
+        });
+      }
+    })
+    .then(function(result) {
+      // After we've either added or removed something from the database,
+      // let's get the TOTAL count of favorites so we can properly update
+      // the client side of things.
       return get({
         CommentId: searchObject.CommentId
       });
@@ -40,6 +58,27 @@ var fave = function(searchObject) {
       console.log('this is the error', err);
       throw err;
     });
+
+  //console.log('FIND ONE: ', result);
+  //console.log('HOW MANY RESULTS? ', result.length);
+
+  // return Heart.findOrCreate({
+  //     where: searchObject
+  //   })
+  //   .then(function(favorite) {
+  //     return get({
+  //       CommentId: searchObject.CommentId
+  //     });
+  //   })
+  //   .then(function(faves) {
+  //     // NUMBER OF FAVORITES!
+  //     console.log("GET FAVES: ", faves.length);
+  //     return faves.length;
+  //   })
+  //   .catch(function(err) {
+  //     console.log('this is the error', err);
+  //     throw err;
+  //   });
 };
 
 exports.get = get;
