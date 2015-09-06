dd7f508c626a522b9149fa1a528031da83b3e660
Matthias Gattermeier <matthias.gattermeier@gmail.com>
2015-09-06 15:47:47 -0700

    feat/extension2 adds handlebar templating and getting and posting comments

--- extension/chrome/sideframe.js
+++ extension/chrome/sideframe.js
@@ -28,6 +28,23 @@ document.addEventListener("DOMContentLoaded", function(e) {
   });
 
 
+  // Post new comment
+  document.getElementById('comment-input-field').addEventListener('keydown', function(e) {
+    if (e.keyCode === 13) {
+      // trigger post
+      postComment(document.getElementById('comment-input-field').value);
+      document.getElementById('comment-input-field').value = '';
+    }
+  });
+  document.getElementById('comment-submit-button').addEventListener('click', function() {
+    postComment(document.getElementById('comment-input-field').value);
+    document.getElementById('comment-input-field').value = '';
+  });
+
+  // this can't work, the event listeners need to be atached individually after loading
+  document.getElementsByClassName('heart').addEventListener('click', function() {});
+  document.getElementsByClassName('flag').addEventListener('click', function() {});
+
 
   /***********/
   /* Generic callback passing along an inital round trip message */
@@ -51,6 +68,7 @@ document.addEventListener("DOMContentLoaded", function(e) {
 });
 
 //
+
 
 
 function loadContent(url) {
@@ -79,13 +97,49 @@ function loadContent(url) {
   });
 
   request.done(function(msg) {
-    // we have to use templating here ...
-    console.log(msg);
-    $("#log").html(msg);
+    // clean the DOM
+    $(".cd-panel-content").html('');
+    // compile and append new comments
+    compileAppendComments(msg.comments);
+  });
+
+  request.fail(function(jqXHR, textStatus) {
+    console.log("Request failed: " + textStatus);
+  });
+
+}
+
+// function to post new comments
+function postComment(text) {
+
+  var data = JSON.stringify({
+    url: url,
+    text: text,
+    isPrivate: false
+  });
+
+  var request = $.ajax({
+    url: server + '/api/comments/add',
+    method: "POST",
+    contentType: "application/json",
+    data: data,
+    dataType: 'json'
+  });
+
+  request.done(function(msg) {
+    // compile and append successfully saved and returned message to DOM
+    compileAppendComments(msg.comments);
   });
 
   request.fail(function(jqXHR, textStatus) {
     alert("Request failed: " + textStatus);
   });
+}
 
+
+function compileAppendComments(comments) {
+  var source = $("#comment-entry-template").html();
+  var template = Handlebars.compile(source);
+  var html = template(comments);
+  $(".cd-panel-content").append(html);
 }
