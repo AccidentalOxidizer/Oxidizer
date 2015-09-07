292e8ad1de8a3831dfea00d1b1da91cee05c037a
Dave Schumaker <daveschumaker@gmail.com>
2015-09-07 15:18:51 -0700

    Working on flag controller

--- server/components/flag/flagController.js
+++ server/components/flag/flagController.js
@@ -18,40 +18,67 @@ var get = function(searchObject) {
 
 var flag = function(searchObject) {
 
-  // First: Search if a flag for this particular user and comment combination already exists.
-  // return Flag.findOne({
-  //     where: searchObject
-  //   })
-  //   .then(function(result) {
-  //     console.log('FLAGGING ITEM', result);
-  //     return result;
-  //   })
-  //   .catch(function(err) {
-  //     console.log("Flag error: ", err);
-  //     return;
-  //   });
-
-
-
-  console.log("FLAG OBJECT: ", searchObject);
-  var newFlag = Flag.build(searchObject);
-  return Flag.findOrCreate({
+  //First: Search
+  // if a flag for this particular user and comment combination already exists.
+  console.log('SEARCHING: ', searchObject);
+  return Flag.findOne({
       where: searchObject
     })
-    .then(function(flag) {
-      return get({
+    .then(function(result) {
+      // Check if the result is null. 
+      // If so, this means the user hasn't flagged this item before.
+      if (result === null) {
+        console.log('No existing flags for this user and comment have been found. Creating new flag.');
+        var newFlag = Flag.build(searchObject);
+        return newFlag.save();
+      } else {
+        // User has already flagged this item before. Let's remove the flag!
+        console.log('Flagged result already found. Removing existing result.');
+        return Flag.destroy({
+          where: searchObject
+        });
+      }
+    })
+    .then(function() {
+      //console.log(searchObject.CommentId);
+      var getCount = get({
         CommentId: searchObject.CommentId
       });
-    })
-    .then(function(flags) {
-      // NUMBER OF FAVORITES!
-      console.log("GET FLAGS: ", flags.length);
-      return flags.length;
+
+      if (getCount[0].dataValues === undefined) {
+        getCount = 0;
+      } else {
+        getCount = getCount[0].dataValues.length;
+      }
+
+      console.log('COUNT!!!!: ', getCount);
     })
     .catch(function(err) {
-      console.log('this is the error', err);
-      throw err;
+      console.log("Flag error: ", err);
+      return;
     });
+
+
+
+  // console.log("FLAG OBJECT: ", searchObject);
+  // var newFlag = Flag.build(searchObject);
+  // return Flag.findOrCreate({
+  //     where: searchObject
+  //   })
+  //   .then(function(flag) {
+  //     return get({
+  //       CommentId: searchObject.CommentId
+  //     });
+  //   })
+  //   .then(function(flags) {
+  //     // NUMBER OF FAVORITES!
+  //     console.log("GET FLAGS: ", flags.length);
+  //     return flags.length;
+  //   })
+  //   .catch(function(err) {
+  //     console.log('this is the error', err);
+  //     throw err;
+  //   });
 };
 
 exports.get = get;
