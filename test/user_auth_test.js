a04e00226ad06989d2e1a7397563de84964d21de
Dave Schumaker <daveschumaker@gmail.com>
2015-09-08 19:24:11 -0700

    Add comments to user authentication tests.

--- test/user_auth_test.js
+++ test/user_auth_test.js
@@ -1,23 +1,23 @@
-/* TEST USER AUTHENTICATION */
-//var utils = require('../server/utils');
-//var Comment = require('../server/components/comment');
-//var commentModel = require('../server/components').Comment;
-//var userModel = require('../server/components').User;
-//var urlModel = require('../server/components').Url;
+/**
+ * USER AUTHENTICATION TESTS
+ *
+ * Currently, this test requires you to run it on your
+ * localhost, since there is parameter in the user/passport.js
+ * file that enables local authentication routes provided 
+ * your server's environment is set to "development".
+ */
+
+// LOAD MODULES
 var Promise = require('bluebird');
 var request = Promise.promisifyAll(require('request'));
-//var config = require('../server/config.js').get(process.env.NODE_ENV);
-//var session = require('express-session');
 
-//var cookieJar = request.jar();
-//console.log('COOOOOOKIE!', cookieJar);
+// GLOBAL SETTINGS FOR THIS PARTICULAR TEST
 var testServer = 'http://localhost:3000';
-var cookie;
+var cookie; // Store cookie token from the server after test file logs in.
 
 // Create a new user with proper credentials using LOCAL authentication
 request.postAsync({
     url: testServer + '/api/auth/local',
-    method: 'POST',
     json: {
       name: 'Testy McTesterson',
       email: 'testUser@test.com',
@@ -25,21 +25,26 @@ request.postAsync({
     }
   })
   .then(function(user) {
+    // We've successfully created a new user or logged in as an existing user!
 
-    console.log('New user created!', user);
-
-    // Set cookie for our newly created user.
+    // Set cookie for our newly created user
+    // from response headers. We'll send this to the
+    // server each time we make a new request.
+    // This is what keeps our "robot" user authenticated.
     cookie = user[0].headers['set-cookie'][0];
+
+    // LOGGING / DEBUGGING INFO
     //console.log('COOKIE: ', user[0].headers['set-cookie'][0]);
     //console.log('HEADERS: ', user[0].headers);
     //console.log('RAW HEADERS: ', user[0].rawHeaders);
     //console.log('Attempting to add a new comment');
 
-    // Let's create a new comment somewhere!
+    // Test URL: This could be any valid URL.
     var testUrl = "http://zombo.com/";
+
+    // CREATE A NEW COMMENT!
     return request.postAsync({
       url: testServer + '/api/comments/add',
-      method: 'POST',
       headers: {
         'Cookie': cookie
       },
