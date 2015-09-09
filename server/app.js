b144a483f57a8d620c8238478f4e8df03c652a86
Matthias Gattermeier <matthias.gattermeier@gmail.com>
2015-09-09 15:34:28 -0700

    implements https support for ssl enabled production server

--- server/app.js
+++ server/app.js
@@ -1,5 +1,7 @@
 // EXPRESS
 var express = require('express');
+var http = require('http');
+var https = require('https');
 var app = express();
 
 // SERVE STATIC FILES FROM CLIENT FOLDER
@@ -18,12 +20,11 @@ var passport = require('passport');
 var sequelize = require('./components').sequelize;
 
 // UTILITIES
+var fs = require('fs');
 var utils = require('./utils');
 
 // CONFIG
 var config = require('./config.js').get(process.env.NODE_ENV);
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000);
-app.set('ip', process.env.OPENSHIFT_NODEJS_IP || process.env.IP || '127.0.0.1');
 
 // initialize passport settings
 require('./components/user/passport')(passport, config);
@@ -51,16 +52,27 @@ sequelize.sync().then(function() {
 
   routes(express, app, passport);
 
-  app.listen(app.get('port'), app.get('ip'), function(err) {
-    if (err) console.log(err);
-    console.log('Server running on port ' + app.get('port') + ' on ' + app.get('ip'));
-  });
+  http.createServer(app, function(req, res) {
+    console.log('http running..');
+  }).listen(80);
 
-  /*
+  if (process.env.NODE_ENV === 'production') {
+    var options = {
+      key: fs.readFileSync(__dirname + '/../keys/key.pem', 'utf-8'),
+      cert: fs.readFileSync(__dirname + '/../keys/api_oxidizer_io.crt', 'utf-8')
+    };
 
-  Warning: connect.session() MemoryStore is not
-  designed for a production environment, as it will leak
-  memory, and will not scale past a single process.
-
-  */
+    https.createServer(options, app, function(req, res) {
+      console.log('https running..');
+    }).listen(443);
+  }
 });
+
+
+/*
+
+Warning: connect.session() MemoryStore is not
+designed for a production environment, as it will leak
+memory, and will not scale past a single process.
+// Maybe redis store or mongostore..
+*/
