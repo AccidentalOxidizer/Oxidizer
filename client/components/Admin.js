452bfa228fa5437b67097db890b01887c1ada236
Dave Schumaker <daveschumaker@gmail.com>
2015-09-09 10:57:51 -0700

    Scaffolding for authentication mixin

--- client/components/Admin.js
+++ client/components/Admin.js
@@ -1,11 +1,21 @@
 var React = require('react');
-
-var React = require('react');
 var Comment = require('./Comment');
+
+// Create Mixin to check that user has the correct credentials to access this page.
+var RoleRequiredMixin = {
+  permitted: function (requiredRoles) {
+    console.log("Heeeeey");
+    // return this.props.profile.roles
+    //   .some(role =>
+    //     requiredRoles.some(rr => rr === role)
+    //   );
+  }
+};
 
 // At the moment, Profile will only be used to display your personal
 // profile, not that of others.
 var Admin = React.createClass({
+  mixins: [RoleRequiredMixin], // Use the mixin
   getInitialState: function() {
     return {
     };
