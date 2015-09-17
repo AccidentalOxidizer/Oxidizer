00ab3570259a96f5560e9416b8865fdf7712bfdf
Dave Schumaker <daveschumaker@gmail.com>
2015-09-16 17:25:17 -0700

    isAdmin middleware now properly checks if logged in user is an administrator.

--- server/middleware/isAdmin.js
+++ server/middleware/isAdmin.js
@@ -1,3 +1,29 @@
+var User = require('../components/user');
+
 module.exports = function(req, res, next) {
-  return false;
-}
+
+  if (req.user === undefined) {
+    res.send(403);
+    return;
+  }
+
+  User.get({id: req.user.id})
+    .then(function(user){
+      if (user.status === 10) {
+        // USER IS ADMIN!
+        next();
+        return;
+      } else {
+        // User is not an admin.
+        // Get out of here, you dirty scoundrel.
+        res.send(403);
+        return;
+      }
+    })
+    .catch(function(){
+      res.send(404);
+      return;
+    });
+
+  //return false;
+};
