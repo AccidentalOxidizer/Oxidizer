ee5e7e4847410c7dc341987538ec3f05a979d351
Dave Schumaker <daveschumaker@gmail.com>
2015-09-17 15:34:58 -0700

    Banned users can no longer conduct actions on server.

--- server/middleware/isAuthorized.js
+++ server/middleware/isAuthorized.js
@@ -1,6 +1,30 @@
+var User = require('../components/user');
 // e.g. Can the user perform this action, like update a profile for another user
 
 module.exports = function(req, res, next) {
   // check if user is himself?
-  next();
-}
+
+  if (req.user === undefined) {
+    res.send(403);
+    return;
+  }
+
+  // Make sure user isn't banned (status === -1), otherwise, they can move on!
+  User.get({id: req.user.id})
+    .then(function(user){
+      if (user.status >= 0) {
+        // USER IS VALID!
+        next();
+        return true;
+      } else {
+        // User is not a valid.
+        // Get out of here, you dirty scoundrel.
+        res.send(403);
+        return false;
+      }
+    })
+    .catch(function(){
+      res.send(404);
+      return false;
+    });
+};
