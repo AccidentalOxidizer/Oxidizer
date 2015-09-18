cb5199adc542973e8a62df3240123ea1e2290ee1
Dave Schumaker <daveschumaker@gmail.com>
2015-09-17 19:47:15 -0700

    Add support for images in React.

--- client/components/Comment.js
+++ client/components/Comment.js
@@ -24,6 +24,42 @@ var Comment = React.createClass({
     };
   },
 
+  findImages: function(text) {
+
+    console.log('TEEEEEXT', text);
+
+    // RegEx for matching image URLs
+    var imagePattern = /\b(https?:\/\/\S+(?:png|jpe?g|gif)\S*)\b/igm;
+
+    // Create an array of matching image URLs
+    var isImageLink = text.match(imagePattern);
+
+    // Let's eliminate any duplicate items in our array so things don't get too crazy.
+    // Also, if we have the same URL In our array multiple times, things will break!
+    if (isImageLink !== null && isImageLink.length > 0) {
+      // Create an array of nonDuplicate image links.
+      var nonDuplicateImages = [];
+
+      isImageLink.forEach(function(element, index) {
+        if (nonDuplicateImages.indexOf(element) === -1) {
+          nonDuplicateImages.push(element);
+        }
+      });
+
+      // Iterate through our array of unique URLs and replace matching image URLs
+      // with HTML img tags.
+      nonDuplicateImages.forEach(function(imageLink) {
+        // Since we want to replace all instances of the URL,
+        // we need to create a regEx object and tell it to look
+        // for all instances that match within the string using the "/g" modifier.
+        var replaceURL = new RegExp(imageLink, 'g');
+        text = text.replace(replaceURL, '<p align="center"><img src="' + imageLink + '" style="max-width: 450px;"/></p>');
+      });
+    }
+
+    return text;
+  },
+
   getReplies: function(){
     var query = {
       repliesToId: this.props.comment.id
@@ -73,6 +109,8 @@ var Comment = React.createClass({
 
     var isPersonalProfile = false;
 
+    var parseTextForImages = this.findImages(this.props.comment.text);
+
     var replies = this.state.replies.map(function(comment, i) {
       return <Comment key={comment.id} comment={comment}  />;
     }.bind(this));
@@ -87,7 +125,7 @@ var Comment = React.createClass({
         </div>
        
         <div className="row"> 
-          <div className="col-xs-12 comment-body">{this.props.comment.text}</div>
+          <div className="col-xs-12 comment-body" dangerouslySetInnerHTML={{__html: parseTextForImages}}></div>
         </div>
         <div className="row">
         <div className="col-xs-12 comment-footer">
